55475e7361eee57b3e9b7ca71b63d6ed4fc5f840
Marcley <marcleyrosa@gmail.com>
2022-08-02 19:11:28 -0300

    Requisito 4: Crie os links de navegação no cabeçalho

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
+          <Route path="/album/:id" component={ Album } />
           <Route exact path="/favorites" component={ Favorites } />
           <Route exact path="/profile" component={ Profile } />
           <Route exact path="/profile/edit" component={ ProfileEdit } />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -24,6 +25,9 @@ export default class Header extends Component {
     return (
       <header data-testid="header-component">
         <p data-testid="header-user-name">{ nameImput }</p>
+        <Link to="/search" data-testid="link-to-search">Voltar para aba de pesquisa</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Musicas favoritas</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
